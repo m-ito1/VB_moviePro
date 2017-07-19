2786a1d19a897531cc2a7f903a201246a4c6eeed
m-ito1 <m-ito@plus-bm.jp>
2017-07-19 17:00:00 +0900

    ユーザーマスタメンテ追加遷移まで
    m-ito1

--- MovieMaster/MovieMaster/viewerMaster.Designer.vb
+++ MovieMaster/MovieMaster/viewerMaster.Designer.vb
@@ -24,18 +24,18 @@ Partial Class viewerMaster
     Private Sub InitializeComponent()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
-        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
-        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.RadioButton4 = New System.Windows.Forms.RadioButton()
         Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Button2 = New System.Windows.Forms.Button()
         Me.Button3 = New System.Windows.Forms.Button()
-        Me.RadioButton4 = New System.Windows.Forms.RadioButton()
         Me.Button4 = New System.Windows.Forms.Button()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
-        Me.TextBox3 = New System.Windows.Forms.TextBox()
-        Me.TextBox4 = New System.Windows.Forms.TextBox()
         Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
         '
@@ -65,16 +65,55 @@ Partial Class viewerMaster
         Me.GroupBox1.TabStop = False
         Me.GroupBox1.Text = " ID   ユーザー名"
         '
-        'RadioButton1
+        'TextBox4
         '
-        Me.RadioButton1.AutoSize = True
-        Me.RadioButton1.Location = New System.Drawing.Point(6, 31)
-        Me.RadioButton1.Name = "RadioButton1"
-        Me.RadioButton1.Size = New System.Drawing.Size(60, 35)
-        Me.RadioButton1.TabIndex = 0
-        Me.RadioButton1.TabStop = True
-        Me.RadioButton1.Text = "01"
-        Me.RadioButton1.UseVisualStyleBackColor = True
+        Me.TextBox4.Location = New System.Drawing.Point(72, 164)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(131, 39)
+        Me.TextBox4.TabIndex = 7
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(72, 119)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(131, 39)
+        Me.TextBox3.TabIndex = 6
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(72, 74)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(131, 39)
+        Me.TextBox2.TabIndex = 5
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(72, 27)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(131, 39)
+        Me.TextBox1.TabIndex = 4
+        '
+        'RadioButton4
+        '
+        Me.RadioButton4.AutoSize = True
+        Me.RadioButton4.Location = New System.Drawing.Point(6, 168)
+        Me.RadioButton4.Name = "RadioButton4"
+        Me.RadioButton4.Size = New System.Drawing.Size(60, 35)
+        Me.RadioButton4.TabIndex = 3
+        Me.RadioButton4.TabStop = True
+        Me.RadioButton4.Text = "04"
+        Me.RadioButton4.UseVisualStyleBackColor = True
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Location = New System.Drawing.Point(6, 123)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(60, 35)
+        Me.RadioButton3.TabIndex = 2
+        Me.RadioButton3.TabStop = True
+        Me.RadioButton3.Text = "03"
+        Me.RadioButton3.UseVisualStyleBackColor = True
         '
         'RadioButton2
         '
@@ -87,16 +126,16 @@ Partial Class viewerMaster
         Me.RadioButton2.Text = "02"
         Me.RadioButton2.UseVisualStyleBackColor = True
         '
-        'RadioButton3
+        'RadioButton1
         '
-        Me.RadioButton3.AutoSize = True
-        Me.RadioButton3.Location = New System.Drawing.Point(6, 123)
-        Me.RadioButton3.Name = "RadioButton3"
-        Me.RadioButton3.Size = New System.Drawing.Size(60, 35)
-        Me.RadioButton3.TabIndex = 2
-        Me.RadioButton3.TabStop = True
-        Me.RadioButton3.Text = "03"
-        Me.RadioButton3.UseVisualStyleBackColor = True
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(6, 31)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(60, 35)
+        Me.RadioButton1.TabIndex = 0
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "01"
+        Me.RadioButton1.UseVisualStyleBackColor = True
         '
         'Button1
         '
@@ -125,17 +164,6 @@ Partial Class viewerMaster
         Me.Button3.Text = "削除"
         Me.Button3.UseVisualStyleBackColor = True
         '
-        'RadioButton4
-        '
-        Me.RadioButton4.AutoSize = True
-        Me.RadioButton4.Location = New System.Drawing.Point(6, 168)
-        Me.RadioButton4.Name = "RadioButton4"
-        Me.RadioButton4.Size = New System.Drawing.Size(60, 35)
-        Me.RadioButton4.TabIndex = 3
-        Me.RadioButton4.TabStop = True
-        Me.RadioButton4.Text = "04"
-        Me.RadioButton4.UseVisualStyleBackColor = True
-        '
         'Button4
         '
         Me.Button4.Location = New System.Drawing.Point(329, 391)
@@ -144,34 +172,6 @@ Partial Class viewerMaster
         Me.Button4.TabIndex = 5
         Me.Button4.Text = "戻る"
         Me.Button4.UseVisualStyleBackColor = True
-        '
-        'TextBox1
-        '
-        Me.TextBox1.Location = New System.Drawing.Point(72, 27)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(131, 39)
-        Me.TextBox1.TabIndex = 4
-        '
-        'TextBox2
-        '
-        Me.TextBox2.Location = New System.Drawing.Point(72, 74)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(131, 39)
-        Me.TextBox2.TabIndex = 5
-        '
-        'TextBox3
-        '
-        Me.TextBox3.Location = New System.Drawing.Point(72, 119)
-        Me.TextBox3.Name = "TextBox3"
-        Me.TextBox3.Size = New System.Drawing.Size(131, 39)
-        Me.TextBox3.TabIndex = 6
-        '
-        'TextBox4
-        '
-        Me.TextBox4.Location = New System.Drawing.Point(72, 164)
-        Me.TextBox4.Name = "TextBox4"
-        Me.TextBox4.Size = New System.Drawing.Size(131, 39)
-        Me.TextBox4.TabIndex = 7
         '
         'viewerMaster
         '
--- MovieMaster/MovieMaster/viewerMaster.vb
+++ MovieMaster/MovieMaster/viewerMaster.vb
@@ -1,11 +1,54 @@
 ﻿Public Class viewerMaster
+    Dim cn As System.Data.SqlClient.SqlConnection
+    Dim command As New SqlClient.SqlCommand
+    Public infoDispList As New List(Of String)
+    Sub viewerMaster_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        infoDispList = getDispInfo()　'データを表示
+        setInfo(infoDispList)
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        forwardScreen(Me, inputViewer) '追加画面に遷移
 
 
-    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+    End Sub
+    Function getDispInfo()
 
-        Dim result As DialogResult = MessageBox.Show(Me, "削除してもよろしいでしょうか", "削除", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation)
+        Dim sr As SqlClient.SqlDataReader
+        Dim infoDispList As New List(Of String)
 
+        'コネクションの指定
+        command.Connection = cn
 
+        'コマンドの種類をテキストにする（省略可）
+        command.CommandType = CommandType.Text
 
+        '実行するSQLを指定
+        command.CommandText = "SELECT USER_ID, USER_NAME FROM M_USER WHERE DELETE_FLAG = 0"
+
+        'SQLの結果を取得する
+        sr = command.ExecuteReader()
+        command.Dispose()
+
+        '取得した結果を出力する
+
+        While sr.Read
+            infoDispList.Add(sr(1))
+        End While
+        sr.Dispose()
+        Return infoDispList
+    End Function
+    Sub setInfo(ByVal infoDispList As List(Of String))
+        TextBox1.Text = infoDispList(0)
+        TextBox2.Text = infoDispList(1)
+        TextBox3.Text = infoDispList(2)
+        TextBox4.Text = infoDispList(3)
+
+    End Sub
+    Public Sub forwardScreen(original As Form, target As Form)
+        Dim inputViewer As New inputViewer
+        target.Show()
+        My.Application.ApplicationContext.MainForm = target
+        original.Dispose()
     End Sub
 End Class
